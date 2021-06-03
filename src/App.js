1915a0ea70dda2a777ea5d52b9bb267df4c8fea2
André Melo <proandremelo@gmail.com>
2021-06-03 17:08:23 -0300

    Primeiros passos

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <section>
+      <form>
+        <input type="text" placeholder="Título"></input>
+        <textarea placeholder="Escreva sua nota..." />
+        <button>Criar Nota</button>
+      </form>
+      
+    </section>
+  );
+}
+
+export default App;
