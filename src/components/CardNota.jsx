fbb8ab9eca0af184b8da3242e4c918310456042d
André Melo <proandremelo@gmail.com>
2021-06-07 20:02:20 -0300

    Integrando o JS

--- src/components/CardNota.jsx
+++ src/components/CardNota.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 class CardNota extends Component {
   render() {
     return (
--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
 import CardNota from "./CardNota";
 
-export default class ListaDeNotas extends Component {
+class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
-        <li>
-          <CardNota />
-        </li>
-        <li>
-          <CardNota />
-        </li>
-        <li>
-          <CardNota />
-        </li>
+        {Array.of("Trabalho", "Trabalho", "Estudo").map((categoria) => {
+          return (
+            <li>
+              <div>{categoria}</div>
+              <CardNota />
+            </li>
+          );
+        })}
       </ul>
     );
   }
 }
+
+export default ListaDeNotas;
