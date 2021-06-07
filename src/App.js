8ade965e1ae00f83eb8779441e16fb80987e96fc
André Melo <proandremelo@gmail.com>
2021-06-07 19:29:50 -0300

    Criando componentes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
-
+import FormularioCadastro from "./components/FormularioCadastro"
 
 function App() {
   return (
     <section>
-      <form>
-        <input type="text" placeholder="Título"></input>
-        <textarea placeholder="Escreva sua nota..." />
-        <button>Criar Nota</button>
-      </form>
+      <FormularioCadastro />
       <ListaDeNotas />
     </section>
   );
